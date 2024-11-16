776644784ac1b9605d86606ad58d6f31fd425656
Prashant K Mali <er.prashantkmrmali@gmail.com>
2024-11-17 02:35:34 +0530

    Data ingestion through MySQL

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 from src.RetailPriceOptimization.logger import logging
 from src.RetailPriceOptimization.exception import CustomException
+from src.RetailPriceOptimization.components.data_ingestion import DataIngestion, DataIngestionConfig
 import sys
 
 if __name__=="__main__":
     logging.info("The Execution has started")
 
     try:
-        pass
+        # data_ingestion_config=DataIngestionConfig()
+        data_ingestion=DataIngestion()
+        data_ingestion.initiate_data_ingestion()
 
     except Exception as e:
         logging.info("Custom Exception")
